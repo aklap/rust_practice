e720fc0b32b57e86ac84f9253736e405a0b9f073
Alexis La Porte <aklaporte@gmail.com>
2017-08-12 19:28:11 -0400

    Add solution for temp conversion exercise

--- 3_control_flow/temp_converter/src/main.rs
+++ 3_control_flow/temp_converter/src/main.rs
@@ -1,15 +1,30 @@
-// Our function that takes a 32bit signed integer as input, performs math and returns celsius as 32bit signed integer
-fn converter(temp: i32) -> i32 {
-    // Bind to variable our math operations with input, 'converted' an immutable variable
-    let converted = (temp - 32) * 5/9;
-    // Implicit return, no semicolon here bc it's an expression not statment
-    converted
+// Solution: Our function takes a 32bit signed integer as input, performs math and returns celsius as 32bit signed integer. We could do floats to be more accurate, but I'm not really concerned here.
+
+// Rust gives us a few options re: dispatch so we have a lot of control here.
+
+// For both solutions, we pass in a closure as an argument. Acc. to docs: 'closures are traits.' A trait is 'a collection of methods defined for an unknown type: Self'
+
+// Static dispatch approach: According to docs, 'Rust favors this.' We assign behavior at compile time. Done through 'monomorphization.' The 'where' clause is how we create a bound for our type, here it's F.
+
+fn convert<F>(measurement: i32, scale_system: F) -> i32 
+    where F: Fn(i32) -> i32 {
+
+    scale_system(measurement)
 }
 
-fn main() {
-    // Test our function works, we expect 29
-    let celsius = converter(85);
+// Dynamic dispatch approach: Rust can also do this. We assign behavior at runtime. Here we reference the trait with &. See comment below on using error messages to reveal this.
 
-    // Print with value of variable we binded using string interpolation 
-    println!("In Celsius that is {}", celsius);
+// fn convert(measurement: i32, scale_system: &Fn(i32) -> i32) -> i32 {
+//     scale_system(measurement)
+// }
+
+fn main() {
+    // Pass in either lambda/closure to the converting function
+    let to_celsius = |temp: i32| -> i32 { (temp - 32) * 5/9 };
+
+    let to_fahrenheit = |temp: i32| -> i32 { (temp * 9/5) + 32 };
+
+    // We pass &closure. Passing to_celsius (no ampersand) results in an error msg: 'expected reference, found closure'. It expects us to reference the closure not pass it in as an arg. We could pass in the closure not binded, as a literal and it would compile. 
+    println!("{} degrees Fahrenheit is {} Celsius", 85, convert(85, &to_celsius));
+    println!("{} degrees Celsius is {} Fahrenheit", 29, convert(29, &to_fahrenheit));
 }
