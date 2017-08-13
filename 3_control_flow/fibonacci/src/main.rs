4afe467d781b174d4e07e561f1956d6ba53028b6
Alexis La Porte <aklaporte@gmail.com>
2017-08-12 20:28:40 -0400

    Add fibonacci solution

---
+++ 3_control_flow/fibonacci/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // I chose to begin my sequence with 0. Sometimes people choose to begin with 1, though. We could have another optional argument for start value of fib sequence.
+
+    fn get_nth(n: i32) -> i32 {
+        if n == 1  {
+            0
+        } else if n == 2 {
+            1
+        } else {
+            get_nth(n-1) + get_nth(n-2)
+        }
+    }
+
+    // Generate the nth Fibonacci number
+    let fib = get_nth(5);
+
+    println!("{}", fib);
+}
+
